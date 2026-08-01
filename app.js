ed08b291ea39484a432cbd356fae5c2403f1996e
zeyad Hamdy <zh9193692@gmail.com>
2026-08-01 21:45:47 +0300

    Connect product routes and fix deployment

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express();
 app.use(express.json());
 
 
+// Routes
+const productRoutes = require("./routes/productRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+
+app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
+
+
 app.get("/", (req, res) => {
   res.json({
     success: true,
