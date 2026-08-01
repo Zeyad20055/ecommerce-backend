b3a41f01fbc5c35681d8abb5430d0f11e215112f
zeyad Hamdy <zh9193692@gmail.com>
2026-08-01 22:01:44 +0300

    Add CORS configuration

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      // ضع رابط الفرونت عند النشر هنا لاحقًا
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
